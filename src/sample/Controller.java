26057645c4317de6034d3086bf8d2d37cfcd45aa
MohammadHossein Dolatabadi <m.h.dolatabadi.a@gmail.com>
2020-05-23 06:08:22 +0430

    implementation base

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,6 @@
 package sample;
 
+import javafx.scene.input.KeyCode;
+
 public class Controller {
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,10 +11,12 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Space Invaders");
+        primaryStage.setScene(new Scene(root, 800, 800));
         primaryStage.show();
     }
+
+
 
 
     public static void main(String[] args) {
