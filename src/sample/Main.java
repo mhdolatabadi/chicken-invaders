afe0d6cf62ab16961551929afd1e3bef5ba0a918
MohammadHossein Dolatabadi <m.h.dolatabadi.a@gmail.com>
2020-05-23 20:16:52 +0430

    start fxml

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,10 +6,17 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.awt.*;
+import java.awt.image.ImageObserver;
+import java.awt.image.ImageProducer;
+import java.io.FileInputStream;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        FileInputStream fileInputStream = new FileInputStream("Untitled.png");
+        Image image = new Image(fileInputStream);
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Space Invaders");
         primaryStage.setScene(new Scene(root, 800, 800));
